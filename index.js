65d77ceec599d5ac07b3036c2855cfe3a50c30d3
Mike Pokalsky <mpoksky1989@gmail.com>
2020-06-20 16:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-function add(a,b) {return a+b}
-function subtract(a,b) {
-  return a-b
-}
-function multiply(a,b) {
-  return a*b
-}
-function divide(a,b) {
-  return a/b
-}
-function increment(n) {
-  return n+=1
-}
-function decrement(n) {
-  return n-=1
-}
-function makeInt(n) {
-  return parseInt(n, 10)
-}
 
 function preserveDecimaal(n) {
   return parseFloat(n)
